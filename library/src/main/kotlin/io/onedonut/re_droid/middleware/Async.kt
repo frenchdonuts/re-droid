5472f64081dc2141a3176af4112ac5856697e470
JT <jonathantan1991@gmail.com>
2016-05-09 17:57:48 -0700

    Renamed async2 to async

--- library/src/main/kotlin/io/onedonut/re_droid/middleware/Async.kt
+++ library/src/main/kotlin/io/onedonut/re_droid/middleware/Async.kt
@@ -13,21 +13,9 @@ open class AsyncEffect(val asyncComputation: Observable<Action>, val cancellatio
 
 class CancelAsyncEffect(val cancellationKey: String, origin: String) : Action(origin)
 
-fun <AppState> async(dispatcher: ((RDB<AppState>, Action) -> Unit)): ((RDB<AppState>, Action) -> Unit) =
-        { rdb, action ->
-            //
-            when (action) {
-                is AsyncEffect -> action.asyncComputation.subscribe { rdb.dispatch(it) }
-                // Ignore all other Actions
-                else -> { }
-            }
-
-            // Run the next dispatcher
-            dispatcher.invoke(rdb, action)
-        }
 
 // :: Map<String, Subscription> -> (Dispatcher -> Dispatcher)
-fun <AppState> async2(subscriptionMap: MutableMap<String, Subscription>): ((RDB<AppState>, Action) -> Unit) -> ((RDB<AppState>, Action) -> Unit) =
+fun <AppState> async(subscriptionMap: MutableMap<String, Subscription>): ((RDB<AppState>, Action) -> Unit) -> ((RDB<AppState>, Action) -> Unit) =
         { dispatcher ->
             { rdb, action ->
                 //
@@ -36,12 +24,25 @@ fun <AppState> async2(subscriptionMap: MutableMap<String, Subscription>): ((RDB<
                         subscriptionMap.put(action.cancellationKey, action.asyncComputation.subscribe { rdb.dispatch(it) })
                     is CancelAsyncEffect ->
                         if (subscriptionMap.containsKey(action.cancellationKey))
-                            subscriptionMap.get(action.cancellationKey)?.unsubscribe()
+                            subscriptionMap[action.cancellationKey]?.unsubscribe()
                     // Ignore all other Actions
                     else -> { }
                 }
 
-                // Run the next dispatcher
+                // Run the next Dispatcher
                 dispatcher.invoke(rdb, action)
             }
         }
+
+/*fun <AppState> async(dispatcher: ((RDB<AppState>, Action) -> Unit)): ((RDB<AppState>, Action) -> Unit) =
+        { rdb, action ->
+            //
+            when (action) {
+                is AsyncEffect -> action.asyncComputation.subscribe { rdb.dispatch(it) }
+            // Ignore all other Actions
+                else -> { }
+            }
+
+            // Run the next dispatcher
+            dispatcher.invoke(rdb, action)
+        }*/
